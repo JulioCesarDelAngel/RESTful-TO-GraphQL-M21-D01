fbe2ab86f176ba73fbdc92991f3b843f3b275a79
Julio C. MW11 <sysa.mail@gmail.com>
2023-03-01 23:45:26 -0600

    se agregan typedef y resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -9,19 +9,39 @@ const typeDefs = gql`
     _id: ID
     username: String
     email: String
-    password: String    
+    bookCount:Int
     savedBooks: [Book]!
   }
 
   type Book {
+    bookId: string
     authors: [String]
     description: String
-    bookId: String
+    title: String
     image: String
     link : String
-    title: String
   }
 
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    bookId: string
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link : String
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!):User
+  }
 
   `;
   
