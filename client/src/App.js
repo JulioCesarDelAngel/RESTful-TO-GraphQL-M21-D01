e2d53d7301b44c37918c2e60e7529b71efb03c3d
Julio C. MW11 <sysa.mail@gmail.com>
2023-03-01 19:25:02 -0600

    se agrega Routes, y configura conexion

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
@@ -7,13 +7,13 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <Router>
-      <>
+      <>21
         <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
+        <Routes>
+          <Route exact path='/' element={<SearchBooks/>} />
+          <Route exact path='/saved' element={<SavedBooks/>} />
           <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
+        </Routes>
       </>
     </Router>
   );
